8ea6d4b3928b1822d55eaf4b822e4d85ff002b8e
Kieu Thanh Nghi <souleater2222001@gmail.com>
2026-01-15 21:11:56 +0700

    next

--- Assets/_asset/Scrips2/ZombieSkill/RushSkill.cs
+++ Assets/_asset/Scrips2/ZombieSkill/RushSkill.cs
@@ -1,48 +1,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
-public class RushSkill : MonoBehaviour
+public class RushSkill : EnemySkill
 {
     [SerializeField] Transform self;
     [SerializeField] SphereCollider coll;
     [SerializeField] ParticleSystem DashMaker;
-    [SerializeField] internal float coolDown = 3;
+    [SerializeField] float markerScaleTime = 1.5f;
+    [SerializeField] float rushTime = 1.5f;
 
-    private void OnEnable()
+    protected void OnEnable()
     {
         coll.enabled = true;
     }
-    private void OnTriggerStay(Collider other)
+    protected void OnTriggerStay(Collider other)
     {
         coll.enabled = false;
         Vector3 targetPos = other.transform.position;
         DashMaker.gameObject.SetActive(true);
         float distance = CalculateDistanceToTarget(targetPos);
-        DOTween.Sequence()
-        .Append(DashMaker.transform.DOScaleX(distance, 1.5f)
-            .OnComplete(() => { 
-                DashMaker.gameObject.SetActive(false);
-                DashMaker.transform.localScale = Vector3.one;
-            }))
-        .Append(self.DOMove(targetPos, 1.5f))
-        .OnComplete(() => {
-            Invoke(nameof(endSkill), coolDown);
-        });
+        Sequence doingSkill = DoSkill(targetPos, distance);
+        AfterSkill(doingSkill);
     }
 
-    float CalculateDistanceToTarget(Vector3 targetPos)
+    protected virtual Sequence DoSkill(Vector3 targetPos, float distance)
+    {
+        return DOTween.Sequence()
+            .Append(DashMaker.transform.DOScaleX(distance, markerScaleTime)
+                .OnComplete(() =>
+                {
+                    DashMaker.gameObject.SetActive(false);
+                    DashMaker.transform.localScale = Vector3.one;
+                }))
+            .Append(self.DOMove(targetPos, rushTime));
+    }
+
+    protected virtual float CalculateDistanceToTarget(Vector3 targetPos)
     {
         return (transform.position - targetPos).magnitude;
     }
-    
-    void endSkill()
+
+    protected virtual void AfterSkill(Sequence doingSkill)
+    {
+        doingSkill.AppendInterval(coolDown).OnComplete(() => EndSkill());
+    }
+
+    protected virtual void EndSkill()
     {
         coll.enabled = true;
     }
+}
 
-    private void OnDisable()
+public class BossRushSkill : RushSkill
+{
+    protected override void AfterSkill(Sequence doingSkill)
     {
-        CancelInvoke();
+        doingSkill.OnComplete(() => gameObject.SetActive(false));
+    }
+}
+
+public class EnemySkill : MonoBehaviour
+{
+    [SerializeField] internal float coolDown = 3;
+    internal virtual void ActiveSkill()
+    {
+        gameObject.SetActive(true);
+    } 
+}
+public class BossSkillManager : MonoBehaviour
+{
+    [SerializeField] EnemySkill[] enemySkills;
+    [SerializeField] Enemy enemySelf;
+    
+    IEnumerator DoSkills()
+    {
+        foreach(var enemySkill in enemySkills)
+        {
+            enemySkill.ActiveSkill();
+            yield return new WaitUntil(() => enemySkill.gameObject.activeSelf == false);
+            yield return new WaitForSeconds(enemySkill.coolDown);
+        }
     }
 }
