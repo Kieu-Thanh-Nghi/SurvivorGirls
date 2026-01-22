2deaf47a73e9bdee728f50e58bd447b8637c64c6
Kieu Thanh Nghi <souleater2222001@gmail.com>
2026-01-22 17:12:49 +0700

    tiếp tục làm ui phần menu

---
+++ Assets/_asset/Scrips2/SelectTabSize.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SelectTabSize")]
+public class SelectTabSize : ScriptableObject
+{
+    [SerializeField] internal float MinX = -13.5f;
+    [SerializeField] internal float MaxX = 13.5f;
+    [SerializeField] internal float MaxY = 28.5f;
+}
---
+++ Assets/_asset/Scrips2/TabSizeChanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TabSizeChanger : MonoBehaviour
+{
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] SelectTabSize tabSize;
+
+    [ContextMenu("test")] 
+    public void SetTabSize()
+    {
+        Vector2 offsetMin = rectTransform.offsetMin;
+        Vector2 offsetMax = rectTransform.offsetMax;
+        offsetMin.x = tabSize.MinX;
+        offsetMax.x = tabSize.MaxX;
+        offsetMax.y = tabSize.MaxY;
+
+        rectTransform.offsetMin = offsetMin;
+        rectTransform.offsetMax = offsetMax;
+    }
+}
